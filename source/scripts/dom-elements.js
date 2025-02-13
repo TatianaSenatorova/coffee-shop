57362c78aa8a5c4713891079dc855de7a2024bc1
TatianaSenatorova <108460915+TatianaSenatorova@users.noreply.github.com>
2025-02-13 21:42:56 +0300

    feat: добавляет слайдер

---
+++ source/scripts/dom-elements.js
@@ -0,0 +1,4 @@
+// export burger = document.querySelector('.main-header__burger');
+// export const mainHeader = document.querySelector('.main-header');
+
+
---
+++ source/scripts/main.js
@@ -0,0 +1,6 @@
+/* в этот файл добавляет скрипты*/
+// import {
+//   mainHeader
+// } from './dom-elements.js';
+
+// mainHeader.classList.remove('main-header--nojs');
