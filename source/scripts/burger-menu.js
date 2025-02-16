7a1fde1042b53f8ff1431cf225671ebb078938c5
TatianaSenatorova <108460915+TatianaSenatorova@users.noreply.github.com>
2025-02-16 14:40:17 +0300

    feat:  добавлена карта

--- source/scripts/burger-menu.js
+++ source/scripts/burger-menu.js
@@ -3,8 +3,6 @@ import {
   burger
 } from './dom-elements';
 
-mainHeader.classList.remove('main-header--nojs');
-
 const onBurgerClick = () => {
   mainHeader.classList.toggle('main-header--nav-is-opened');
 };
