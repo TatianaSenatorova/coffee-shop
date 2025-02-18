bf2fc067dd9a26cb4e7875c3b551b1696592c9c4
TatianaSenatorova <108460915+TatianaSenatorova@users.noreply.github.com>
2025-02-18 12:15:42 +0300

    fix

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -1,4 +1,3 @@
 import './burger-menu.js';
-import './select.js';
 import './slider-range.js';
 import './form.js';
--- source/scripts/slider-range.js
+++ source/scripts/slider-range.js
@@ -1,7 +1,7 @@
 import { sliderInitValues } from './constants';
 import { slider, minPriceInput, maxPriceInput } from './dom-elements.js';
 
-noUiSlider.cssClasses.target += 'range';
+// noUiSlider.cssClasses.target += 'range';
 
 noUiSlider.create(slider, {
   range: {
@@ -19,47 +19,10 @@ noUiSlider.create(slider, {
       return parseFloat(value);
     },
   },
-  cssClasses: {
-    target: 'target',
-    base: 'base',
-    origin: 'origin',
-    handle: 'testHandle',
-    handleLower: 'handle-lower',
-    handleUpper: 'handle-upper',
-    touchArea: 'touch-area',
-    horizontal: 'horizontal',
-    vertical: 'vertical',
-    background: 'background',
-    connect: 'connect',
-    connects: 'connects',
-    ltr: 'ltr',
-    rtl: 'rtl',
-    textDirectionLtr: 'txt-dir-ltr',
-    textDirectionRtl: 'txt-dir-rtl',
-    draggable: 'draggable',
-    drag: 'state-drag',
-    tap: 'state-tap',
-    active: 'active',
-    tooltip: 'tooltip',
-    pips: 'pips',
-    pipsHorizontal: 'pips-horizontal',
-    pipsVertical: 'pips-vertical',
-    marker: 'marker',
-    markerHorizontal: 'marker-horizontal',
-    markerVertical: 'marker-vertical',
-    markerNormal: 'marker-normal',
-    markerLarge: 'marker-large',
-    markerSub: 'marker-sub',
-    value: 'value',
-    valueHorizontal: 'value-horizontal',
-    valueVertical: 'value-vertical',
-    valueNormal: 'value-normal',
-    valueLarge: 'value-large',
-    valueSub: 'value-sub',
-  },
 });
 
 slider.noUiSlider.on('update', (values) => {
+  console.log(slider, values, minPriceInput, maxPriceInput);
   minPriceInput.value = values[0];
   maxPriceInput.value = values[1];
 });
