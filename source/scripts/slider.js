7fc73251d24386e529b46456aa7d78b4f6400155
Tatiana Senatorova <1975tanya@bk.ru>
2025-05-16 18:16:12 +0300

    fix: добавляет resizeObserver для отслеживания ширины слайдера при изменении окна браузера. Убирает из фокуса карточки слайдера, которые в данный момент не на экране

--- source/scripts/slider.js
+++ source/scripts/slider.js
@@ -8,27 +8,61 @@ import {
   sliderArrowButtons
 } from './dom-elements.js';
 
-const slideWidth = sliderContainer.offsetWidth;
+let slideWidth = sliderContainer.offsetWidth;
 let currentIndex = 0;
 sliderPrevButton.disabled = true;
 
-const updateSlider = () => {
-  sliderArrowButtons.forEach((button) =>{
-    button.disabled = false;
+const transferSlider = () => {
+  sliderList.style.transform = `translateX(${-slideWidth * currentIndex}px)`;
+}
+
+const resizeObserver = new ResizeObserver((entries) => {
+  for (const entry of entries) {
+    if (entry.target.classList.contains('slider')) {
+      slideWidth = entry.contentRect.width;
+      transferSlider();
+    }
   }
-  );
-  if(currentIndex === 0) {
-    sliderPrevButton.disabled = true;
-  } else if(currentIndex === sliderItems.length - 1) {
-    sliderNextButton.disabled = true;
-  }
+});
+
+const observeSliderWidth = () => {
+  resizeObserver.observe(sliderContainer);
+};
+
+observeSliderWidth();
+
+const changeClasses = () => {
   sliderPaginationButtons.forEach((button) =>
     button.classList.remove('slider__pagination-button--is-current')
   );
   sliderPaginationButtons[currentIndex].classList.add(
     'slider__pagination-button--is-current'
   );
-  sliderList.style.transform = `translateX(${-slideWidth * currentIndex}px)`;
+}
+
+const unfocusNonActiveSlides = () => {
+  sliderItems.forEach((slide, index) => {
+    if (index === currentIndex) {
+      slide.querySelector('.slider-card__button').removeAttribute('tabindex');
+    } else {
+      slide.querySelector('.slider-card__button').setAttribute('tabindex', '-1');
+    }
+  });
+};
+
+const updateSlider = () => {
+  sliderArrowButtons.forEach((button) => {
+    button.disabled = false;
+  }
+  );
+  if (currentIndex === 0) {
+    sliderPrevButton.disabled = true;
+  } else if (currentIndex === sliderItems.length - 1) {
+    sliderNextButton.disabled = true;
+  }
+  changeClasses();
+  transferSlider();
+  unfocusNonActiveSlides();
 };
 
 sliderPaginationButtons.forEach((button, index) => {
@@ -55,7 +89,7 @@ const onDocumentKeyDown = (evt) => {
   if (evt.key === 'ArrowRight' && currentIndex !== (sliderItems.length - 1)) {
     currentIndex++;
     updateSlider();
-  } else if (evt.key === 'ArrowLeft' && currentIndex !== 0){
+  } else if (evt.key === 'ArrowLeft' && currentIndex !== 0) {
     currentIndex--;
     updateSlider();
   }
